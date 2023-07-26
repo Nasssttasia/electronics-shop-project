f9afb2e63041c053e10af0dfb628a8f9a8db7b34
Anastasia Chernysheva <nastya770098@gmail.com>
2023-07-26 19:03:57 +0300

    hw1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,11 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+def test_calculate_total_price(item3):
+    assert item3.calculate_total_price() == 200000
+
+def test_apply_discount(item3):
+    Item.pay_rate = 0.8
+    assert item3.price == 10000
+
