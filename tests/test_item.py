3fd894005e37a209fed071d3bdd6cff0f336bdd9
Anastasia Chernysheva <nastya770098@gmail.com>
2023-07-27 16:59:38 +0300

    hw1

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,10 +2,11 @@ import pytest
 
 from src.item import Item
 
+
 def test_calculate_total_price(item3):
     assert item3.calculate_total_price() == 200000
+
 
 def test_apply_discount(item3):
     Item.pay_rate = 0.8
     assert item3.price == 10000
-
