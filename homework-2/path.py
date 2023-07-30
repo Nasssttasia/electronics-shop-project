b6cfb355eee82b9a947b9a906b228a3743a3d9ae
Anastasia Chernysheva <nastya770098@gmail.com>
2023-07-30 18:18:18 +0300

    hw2 not done, without tests

---
+++ homework-2/path.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+SRC_PATH = Path.joinpath(ROOT, 'src')
+ITEMS_PATH = Path.joinpath(SRC_PATH, 'items.csv')
+
+
+
--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -5,7 +8,7 @@ class Item:
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
+    def __init__(self, name, price, quantity: int) -> None:
         """
         Создание экземпляра класса item.
 
@@ -13,10 +16,34 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
         self.all.append(self)
+
+    @property
+    def name(self):
+        """возвращает name"""
+        return self.__name
+
+    @name.setter
+    def name(self, user_input):
+        if len(user_input) < 10:
+            self.name = user_input
+        else:
+            self.name = user_input[0:9]
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        with open(path, "r", encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for i in reader:
+                name, price, quantity = i['name'], float(i['price']), int(i['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(str_):
+        return int(float(str_))
 
     def calculate_total_price(self) -> float:
         """
