bf4ebc14147931b2be180d286d8eb6a98fcd3734
Anastasia Chernysheva <nastya770098@gmail.com>
2023-08-08 22:04:39 +0300

    hw4

--- src/phone.py
+++ src/phone.py
@@ -5,7 +5,16 @@ class Phone(Item):
 
     def __init__(self, name, price, quantity, number_of_sim):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, number_of_sim):
+        self.__number_of_sim = number_of_sim
+
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
