b286e8192bc986e70b9dcb4f9b0e232e7615529d
Anastasia Chernysheva <nastya770098@gmail.com>
2023-08-09 23:44:17 +0300

    hw5

---
+++ tests/test_keyboard.py
@@ -0,0 +1,13 @@
+from src.keyboard import Keyboard
+
+keyboard3 = Keyboard("iPhone 14", 120_000, 5)
+
+def test_initial_keyboard(keyboard3):
+    assert keyboard3.price == 120_000
+    assert keyboard3.name == "iPhone 14"
+    assert keyboard3.quantity == 5
+    assert keyboard3.language == 'EN'
+
+def test_language(keyboard3):
+    keyboard3.change_lang()
+    assert str(keyboard3.language) == "RU"
